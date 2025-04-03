bd86aaa9d33b99ecd80ca79d6c11e76063f41f86
Saider <pilipenkro@gmail.com>
2025-04-03 20:21:04 +0300

    CORS settings

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,10 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RefreshResponse } from './refreshApi';
 
+
+export interface AuthData {
+    email: string; 
+    password: string; 
+  }
+  
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ baseUrl: "https://pilipenkoaleksey.ru/api/v1/auth" }),
   endpoints: (builder) => ({
-    login: builder.mutation({
+    login: builder.mutation<RefreshResponse, AuthData >({
       query: (credentials) => ({
         url: '/authorize',
         method: 'POST',
--- client/src/pages/SshConnectionPage/LoginPage.tsx
+++ client/src/pages/SshConnectionPage/LoginPage.tsx
@@ -1,41 +1,56 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import { useLoginMutation } from '../../api/authApi';
 import { useDispatch } from 'react-redux';
-import { clearTokens,  setTokens } from '../../slice/authSlice';
+import { clearTokens, setTokens } from '../../slice/authSlice';
 import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
-  const { register, handleSubmit } = useForm<{ username: string; password: string }>();
+  const [email, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [login, { isLoading }] = useLoginMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const onSubmit = async (data: { username: string; password: string }) => {
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    
     try {
-     
-      const result = await login(data).unwrap();
-      console.log("Login successful!", result);
+      const result = await login({ email, password }).unwrap();
+      console.log('Login successful!', result);
       dispatch(clearTokens());
       dispatch(setTokens({
         accessToken: result.access_token,
         refreshToken: result.refresh_token,
       }));
-     
-      navigate("/"); 
+      
+      navigate('/');
     } catch (err) {
-      console.error("Login failed:", err);
-      alert("Ошибка авторизации");
+      console.error('Login failed:', err);
+      console.log(email);
+      console.log(password);
+      alert('Ошибка авторизации');
     }
   };
 
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('username')} placeholder='Username' required />
-        <input {...register('password')} type='password' placeholder='Password' required />
-        <button type='submit' disabled={isLoading}>Login</button>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={email}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username" 
+          required 
+        />
+        <input 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password" 
+          required 
+        />
+        <button type="submit" disabled={isLoading}>Login</button>
       </form>
     </div>
   );
